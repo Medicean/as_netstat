bfef2575191546bdde606eb92a6e6822d5010e25
Medicean <Medici.Yan@Gmail.com>
2019-01-28 10:20:00 +0800

    add language

---
+++ language/en.js
@@ -0,0 +1,28 @@
+module.exports = {
+  title: "NetStat",
+  success: "Ret Success",
+  error: "Ret Error",
+  main: {
+    toolbar: {
+      label: 'Net Config',
+      tcp4: 'Get tcp4 connection information',
+      udp4: 'Get udp4 connection information',
+      start: 'Get',
+      manual: 'Manual Input',
+      clear: 'Clear',
+    },
+  },
+  manual: {
+    title: (path)=> `Paste the content in ${path}`,
+    toolbar: {
+      label: 'INET Type',
+      save: 'Parse'
+    },
+  },
+  msg: {
+    onlylinux: "Only support Linux Shell",
+    emptyselect: "Choose at least one type",
+    empty: "Please complete the form",
+    parse_finished: "Parsed",
+  },
+}
---
+++ language/zh.js
@@ -0,0 +1,6 @@
+module.exports = {
+    title: "网络连接",
+    success: "获取成功",
+    error: "获取失败",
+    
+}
